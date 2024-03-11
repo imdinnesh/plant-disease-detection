e7017fe10dcac9532ed65f9bc88c13d72844f8a3
imdinnesh <itsdinnesh@gmail.com>
2024-03-11 22:35:57 +0530

    Fixed homepage bug

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -12,38 +12,38 @@ function HomaPage() {
     const firebase = useFirebase()
 
     useEffect(() => {
-        if (firebase.isloggedIn) {
+        if (!firebase.isloggedIn) {
             setActive(!active)
         }
         else {
-            setActive(false)
+            setActive(true)
         }
     }, [firebase.isloggedIn])
 
     console.log(active);
 
-    if (active) {
+    if (!active) {
         return (
-            <div className='mt-36'>
-                <NavBar/>
-                <div className='flex'>
-                    <div className='w-1/2 p-9'>
-                        <h1 className='text-5xl font-bold font-mono'><span className='text-green-600'>Plant</span> Disease
-                            <br />
-                            <span className='text-red-600'>Detection</span> & Monitoring</h1>
-                        <p className='mt-10 font-semibold text-xl'>Just drag and drop image of leaves to detect plant disease</p>
-                        <button className=' mt-8 w-36 h-10 px-2 py-1 bg-slate-700 rounded-md text-white'>Get Started</button>
-                    </div>
-                    <div className='w-1/2'>
-                        <DragDrop />
-                    </div>
-                </div>
-                <Footer/>
-            </div>
+            <LobbyScreen/>
         )
     }
     else{
-        return <LobbyScreen/>
+        return (<div className='mt-36'>
+        <NavBar/>
+        <div className='flex'>
+            <div className='w-1/2 p-9'>
+                <h1 className='text-5xl font-bold font-mono'><span className='text-green-600'>Plant</span> Disease
+                    <br />
+                    <span className='text-red-600'>Detection</span> & Monitoring</h1>
+                <p className='mt-10 font-semibold text-xl'>Just drag and drop image of leaves to detect plant disease</p>
+                <button className=' mt-8 w-36 h-10 px-2 py-1 bg-slate-700 rounded-md text-white'>Get Started</button>
+            </div>
+            <div className='w-1/2'>
+                <DragDrop />
+            </div>
+        </div>
+        <Footer/>
+    </div>)
     }
 
 
